5c0376313d32c343549f017b139d5c9702a47dcd
Suchandra Thapa <suchandra@gmail.com>
2022-01-21 12:37:06 -1000

    Cleanup and fix k8s calls
    Update main.go to use the latest k8s api code (esp. contexts) and
    clean up code to avoid multiline statements

--- main.go
+++ main.go
@@ -10,7 +10,7 @@ import (
 	"k8s.io/client-go/rest"
 	"k8s.io/client-go/tools/cache"
 
-	nrpapi "nrp-clone/pkg/apis/nrpapi/v1alpha1"
+	nrpapi "github.com/slateci/nrp-clone/pkg/apis/nrpapi/v1alpha1"
 
 	apiextcs "k8s.io/apiextensions-apiserver/pkg/client/clientset/clientset"
 	metaV1 "k8s.io/apimachinery/pkg/apis/meta/v1"
@@ -23,6 +23,191 @@ import (
 var clientset *kubernetes.Clientset
 var clustcrdclient *nrpapi.ClusterCrdClient
 var clustnscrdclient *nrpapi.ClusterNSCrdClient
+
+var clusterControllerHandlers = cache.ResourceEventHandlerFuncs{
+	AddFunc: func(obj interface{}) {
+		cluster, ok := obj.(*nrpapi.Cluster)
+		if !ok {
+			log.Printf("Expected Cluster but other received %#v", obj)
+			return
+		}
+		todoCtx := context.TODO()
+
+		if cluster.Spec.Namespace == "" {
+			clusterNamespace := v1.Namespace{
+				ObjectMeta: metaV1.ObjectMeta{
+					Name: findFreeNamespace(todoCtx, cluster.Name),
+				},
+			}
+			clusterns, err := clientset.CoreV1().Namespaces().Create(todoCtx, &clusterNamespace, metaV1.CreateOptions{})
+			if err != nil {
+				log.Printf("Error creating cluster namespace %s", err.Error())
+				return
+			}
+			serviceAccount := v1.ServiceAccount{
+				ObjectMeta: metaV1.ObjectMeta{
+					Name:      cluster.Name,
+					Namespace: clusterns.Name,
+				},
+			}
+			srvAcc, err := clientset.CoreV1().ServiceAccounts(clusterns.Name).Create(todoCtx, &serviceAccount, metaV1.CreateOptions{})
+			if err != nil {
+				log.Printf("Error creating service account %s", err.Error())
+				return
+			}
+			roleBinding := rbacv1.RoleBinding{
+				ObjectMeta: metaV1.ObjectMeta{
+					Name: cluster.Name,
+				},
+				RoleRef: rbacv1.RoleRef{
+					Kind:     "ClusterRole",
+					Name:     "federation-cluster",
+					APIGroup: "rbac.authorization.k8s.io",
+				},
+				Subjects: []rbacv1.Subject{
+					{
+						Kind: "ServiceAccount",
+						Name: srvAcc.Name,
+					},
+				},
+			}
+			_, err = clientset.RbacV1().RoleBindings(clusterns.Name).Create(todoCtx, &roleBinding, metaV1.CreateOptions{})
+			if err != nil {
+				log.Printf("Error creating federation-cluster rolebinding %s", err.Error())
+			}
+			clusterRoleBinding := rbacv1.ClusterRoleBinding{
+				ObjectMeta: metaV1.ObjectMeta{
+					Name: cluster.Name,
+				},
+				RoleRef: rbacv1.RoleRef{
+					Kind:     "ClusterRole",
+					Name:     "federation-cluster-global",
+					APIGroup: "rbac.authorization.k8s.io",
+				},
+				Subjects: []rbacv1.Subject{
+					{
+						Kind:      "ServiceAccount",
+						Name:      srvAcc.Name,
+						Namespace: clusterns.Name,
+					},
+				},
+			}
+			_, err = clientset.RbacV1().ClusterRoleBindings().Create(todoCtx, &clusterRoleBinding, metaV1.CreateOptions{})
+			if err != nil {
+				log.Printf("Error creating federation-cluster-global clusterrolebinding %s", err.Error())
+			}
+
+			cluster.Spec.Namespace = clusterns.Name
+			if _, err := clustcrdclient.Update(todoCtx, cluster); err != nil {
+				log.Printf("Error updating cluster %s ns %s", cluster.Name, err.Error())
+			}
+		}
+	},
+	DeleteFunc: func(obj interface{}) {
+		cluster, ok := obj.(*nrpapi.Cluster)
+		todoCtx := context.TODO()
+
+		if !ok {
+			log.Printf("Expected Cluster but other received %#v", obj)
+			return
+		}
+		if cluster.Spec.Namespace != "" {
+			if clusterNamespaces, err := clustnscrdclient.List(todoCtx, cluster.Spec.Namespace, metaV1.ListOptions{}); err == nil {
+				for _, clusterNs := range clusterNamespaces.Items {
+					if err := clientset.CoreV1().Namespaces().Delete(todoCtx, clusterNs.Name, metaV1.DeleteOptions{}); err != nil {
+						fmt.Printf("Error deleting clusternamespace %s %s", clusterNs.Name, err.Error())
+					}
+				}
+				if err := clientset.CoreV1().Namespaces().Delete(todoCtx, cluster.Spec.Namespace, metaV1.DeleteOptions{}); err != nil {
+					fmt.Printf("Error deleting cluster namespace %s %s", cluster.Spec.Namespace, err.Error())
+				}
+			}
+
+		}
+	},
+	UpdateFunc: func(oldObj, newObj interface{}) {
+		_, ok := oldObj.(*nrpapi.Cluster)
+		if !ok {
+			log.Printf("Expected Cluster but other received %#v", oldObj)
+			return
+		}
+		_, ok = newObj.(*nrpapi.Cluster)
+		if !ok {
+			log.Printf("Expected Cluster but other received %#v", newObj)
+			return
+		}
+	},
+}
+
+var clusterNSControllerHandlers = cache.ResourceEventHandlerFuncs{
+	AddFunc: func(obj interface{}) {
+		clusterNs, ok := obj.(*nrpapi.ClusterNamespace)
+
+		if !ok {
+			log.Printf("Expected ClusterNamespace but other received %#v", obj)
+			return
+		}
+		todoCtx := context.TODO()
+
+		namespace := v1.Namespace{
+			ObjectMeta: metaV1.ObjectMeta{
+				Name: clusterNs.Name,
+			},
+		}
+		clusterns, err := clientset.CoreV1().Namespaces().Create(todoCtx, &namespace, metaV1.CreateOptions{})
+		if err != nil {
+			log.Printf("Error creating cluster namespace %s", err.Error())
+			return
+		}
+		roleBinding := rbacv1.RoleBinding{
+			ObjectMeta: metaV1.ObjectMeta{
+				Name: clusterNs.Name,
+			},
+			RoleRef: rbacv1.RoleRef{
+				Kind:     "ClusterRole",
+				Name:     "federation-cluster",
+				APIGroup: "rbac.authorization.k8s.io",
+			},
+			Subjects: []rbacv1.Subject{
+				{
+					Kind:      "ServiceAccount",
+					Name:      clusterNs.Namespace,
+					Namespace: clusterNs.Namespace,
+				},
+			},
+		}
+		_, err = clientset.RbacV1().RoleBindings(clusterns.Name).Create(todoCtx, &roleBinding, metaV1.CreateOptions{})
+		if err != nil {
+			log.Printf("Error creating cluster rolebinding %s", err.Error())
+			return
+		}
+
+	},
+	DeleteFunc: func(obj interface{}) {
+		clusterNs, ok := obj.(*nrpapi.ClusterNamespace)
+		if !ok {
+			log.Printf("Expected ClusterNamespace but other received %#v", obj)
+			return
+		}
+		todoCtx := context.TODO()
+		if err := clientset.CoreV1().Namespaces().Delete(todoCtx, clusterNs.Name, metaV1.DeleteOptions{}); err != nil {
+			log.Printf("Error deleting namespace for cluster: %s", err.Error())
+			return
+		}
+	},
+	UpdateFunc: func(oldObj, newObj interface{}) {
+		_, ok := oldObj.(*nrpapi.ClusterNamespace)
+		if !ok {
+			log.Printf("Expected Cluster but other received %#v", oldObj)
+			return
+		}
+		_, ok = newObj.(*nrpapi.ClusterNamespace)
+		if !ok {
+			log.Printf("Expected Cluster but other received %#v", newObj)
+			return
+		}
+	},
+}
 
 func main() {
 	ctx := context.Background()
@@ -67,11 +252,11 @@ func GetCrd(ctx context.Context) {
 		panic(err.Error())
 	}
 
-	if err := nrpapi.CreateClusterCRD(crdclientset); err != nil {
+	if err := nrpapi.CreateClusterCRD(ctx, crdclientset); err != nil {
 		log.Printf("Error creating CRD: %s", err.Error())
 	}
 
-	if err := nrpapi.CreateNSCRD(crdclientset); err != nil {
+	if err := nrpapi.CreateNSCRD(ctx, crdclientset); err != nil {
 		log.Printf("Error creating CRD: %s", err.Error())
 	}
 
@@ -82,175 +267,14 @@ func GetCrd(ctx context.Context) {
 		clustcrdclient.NewListWatch(),
 		&nrpapi.Cluster{},
 		time.Minute*1,
-		cache.ResourceEventHandlerFuncs{
-			AddFunc: func(obj interface{}) {
-				cluster, ok := obj.(*nrpapi.Cluster)
-				if !ok {
-					log.Printf("Expected Cluster but other received %#v", obj)
-					return
-				}
-				if cluster.Spec.Namespace == "" {
-					if clusterns, err := clientset.CoreV1().Namespaces().Create(&v1.Namespace{
-						ObjectMeta: metaV1.ObjectMeta{
-							Name: findFreeNamespace(ctx, cluster.Name),
-						},
-					}); err == nil {
-						if srvAcc, err := clientset.CoreV1().ServiceAccounts(clusterns.Name).Create(&v1.ServiceAccount{
-							ObjectMeta: metaV1.ObjectMeta{
-								Name:      cluster.Name,
-								Namespace: clusterns.Name,
-							},
-						}); err == nil {
-							if _, err := clientset.RbacV1().RoleBindings(clusterns.Name).Create(&rbacv1.RoleBinding{
-								ObjectMeta: metaV1.ObjectMeta{
-									Name: cluster.Name,
-								},
-								RoleRef: rbacv1.RoleRef{
-									Kind:     "ClusterRole",
-									Name:     "federation-cluster",
-									APIGroup: "rbac.authorization.k8s.io",
-								},
-								Subjects: []rbacv1.Subject{
-									rbacv1.Subject{
-										Kind: "ServiceAccount",
-										Name: srvAcc.Name,
-									},
-								},
-							}); err != nil {
-								log.Printf("Error creating federation-cluster rolebinding %s", err.Error())
-							}
-							if _, err := clientset.RbacV1().ClusterRoleBindings().Create(&rbacv1.ClusterRoleBinding{
-								ObjectMeta: metaV1.ObjectMeta{
-									Name: cluster.Name,
-								},
-								RoleRef: rbacv1.RoleRef{
-									Kind:     "ClusterRole",
-									Name:     "federation-cluster-global",
-									APIGroup: "rbac.authorization.k8s.io",
-								},
-								Subjects: []rbacv1.Subject{
-									rbacv1.Subject{
-										Kind:      "ServiceAccount",
-										Name:      srvAcc.Name,
-										Namespace: clusterns.Name,
-									},
-								},
-							}); err != nil {
-								log.Printf("Error creating federation-cluster-global clusterrolebinding %s", err.Error())
-							}
-
-							cluster.Spec.Namespace = clusterns.Name
-							if _, err := clustcrdclient.Update(cluster); err != nil {
-								log.Printf("Error updating cluster %s ns %s", cluster.Name, err.Error())
-							}
-						} else {
-							log.Printf("Error creating service account %s", err.Error())
-							return
-						}
-					} else {
-						log.Printf("Error creating cluster namespace %s", err.Error())
-						return
-					}
-				}
-			},
-			DeleteFunc: func(obj interface{}) {
-				cluster, ok := obj.(*nrpapi.Cluster)
-				if !ok {
-					log.Printf("Expected Cluster but other received %#v", obj)
-					return
-				}
-				if cluster.Spec.Namespace != "" {
-					if clusterNamespaces, err := clustnscrdclient.List(cluster.Spec.Namespace, metaV1.ListOptions{}); err == nil {
-						for _, clusterNs := range clusterNamespaces.Items {
-							if err := clientset.CoreV1().Namespaces().Delete(clusterNs.Name, &metaV1.DeleteOptions{}); err != nil {
-								fmt.Printf("Error deleting clusternamespace %s %s", clusterNs.Name, err.Error())
-							}
-						}
-						if err := clientset.CoreV1().Namespaces().Delete(cluster.Spec.Namespace, &metaV1.DeleteOptions{}); err != nil {
-							fmt.Printf("Error deleting cluster namespace %s %s", cluster.Spec.Namespace, err.Error())
-						}
-					}
-
-				}
-			},
-			UpdateFunc: func(oldObj, newObj interface{}) {
-				_, ok := oldObj.(*nrpapi.Cluster)
-				if !ok {
-					log.Printf("Expected Cluster but other received %#v", oldObj)
-					return
-				}
-				_, ok = newObj.(*nrpapi.Cluster)
-				if !ok {
-					log.Printf("Expected Cluster but other received %#v", newObj)
-					return
-				}
-			},
-		},
+		clusterControllerHandlers,
 	)
 
 	_, clusterNSController := cache.NewInformer(
 		clustnscrdclient.NewListWatch(""),
 		&nrpapi.ClusterNamespace{},
 		time.Minute*1,
-		cache.ResourceEventHandlerFuncs{
-			AddFunc: func(obj interface{}) {
-				clusterNs, ok := obj.(*nrpapi.ClusterNamespace)
-				if !ok {
-					log.Printf("Expected ClusterNamespace but other received %#v", obj)
-					return
-				}
-				if clusterns, err := clientset.CoreV1().Namespaces().Create(&v1.Namespace{
-					ObjectMeta: metaV1.ObjectMeta{
-						Name: clusterNs.Name,
-					},
-				}); err == nil {
-					clientset.RbacV1().RoleBindings(clusterns.Name).Create(&rbacv1.RoleBinding{
-						ObjectMeta: metaV1.ObjectMeta{
-							Name: clusterNs.Name,
-						},
-						RoleRef: rbacv1.RoleRef{
-							Kind:     "ClusterRole",
-							Name:     "federation-cluster",
-							APIGroup: "rbac.authorization.k8s.io",
-						},
-						Subjects: []rbacv1.Subject{
-							rbacv1.Subject{
-								Kind:      "ServiceAccount",
-								Name:      clusterNs.Namespace,
-								Namespace: clusterNs.Namespace,
-							},
-						},
-					})
-				} else {
-					log.Printf("Error creating cluster namespace %s", err.Error())
-					return
-				}
-			},
-			DeleteFunc: func(obj interface{}) {
-				clusterNs, ok := obj.(*nrpapi.ClusterNamespace)
-				if !ok {
-					log.Printf("Expected ClusterNamespace but other received %#v", obj)
-					return
-				}
-
-				if err := clientset.CoreV1().Namespaces().Delete(clusterNs.Name, &metaV1.DeleteOptions{}); err != nil {
-					log.Printf("Error deleting namespace for cluster: %s", err.Error())
-					return
-				}
-			},
-			UpdateFunc: func(oldObj, newObj interface{}) {
-				_, ok := oldObj.(*nrpapi.ClusterNamespace)
-				if !ok {
-					log.Printf("Expected Cluster but other received %#v", oldObj)
-					return
-				}
-				_, ok = newObj.(*nrpapi.ClusterNamespace)
-				if !ok {
-					log.Printf("Expected Cluster but other received %#v", newObj)
-					return
-				}
-			},
-		},
+		clusterNSControllerHandlers,
 	)
 
 	stop := make(chan struct{})
